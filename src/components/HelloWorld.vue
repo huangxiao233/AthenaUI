7ce2eb8bcbe0c221281fd0ea282b88011eeb0116
黄笑 <huangxiao221@jd.com>
2020-08-05 14:27:30 +0800

    小试牛刀

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,8 @@
 <template>
 <h1>{{ msg }}</h1>
-<button @click="count++">count is: {{ count }}</button>
-<p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+
+
+<p>这是雅典娜前端UI组件库</p>
 </template>
 
 <script>
